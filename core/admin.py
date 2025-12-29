b44b063186f79ae9fc436edb2604668924014f82
Icaro Mendes <114268516+icaroMendes777@users.noreply.github.com>
2025-12-29 13:41:24 -0300

    feat: petition crud

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Petition
 
-# Register your models here.
+@admin.register(Petition)
+class PetitionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at")
+    search_fields = ("title",)
+
